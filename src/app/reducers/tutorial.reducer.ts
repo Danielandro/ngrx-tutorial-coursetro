e098cd0a1caf4d2f3c7fca276fc5d81db20f210b
Danny <mtme13@hotmail.com>
2020-02-18 13:34:54 +0000

    Create reducer function

---
+++ src/app/reducers/tutorial.reducer.ts
@@ -0,0 +1,23 @@
+import { Action } from "@ngrx/store";
+import { Tutorial } from "../models/tutorial.model";
+import * as TutorialActions from "../actions/tutorial.action";
+
+// define initial app state
+const initialState: Tutorial = {
+  name: "Initial Tutorial",
+  url: "http://skysports.com"
+};
+
+// Define the reducer - takes state and action as arguments
+// Set initial state
+// Set valid types for actions
+export function reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
+  //check type of action
+  switch (action.type) {
+    // if type = "[TUTORIAL] Add", return [current state + new tutorial]
+    case TutorialActions.ADD_TUTORIAL:
+      return [...state, action.payload];
+    // return current state by default if action type doesn't match any cases
+    default: return state;
+  }
+}
